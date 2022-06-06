6a7640c2d5676572e8de87ce83ad506fd2972ac2
Jackson Bailey <Bailey95jackson@gmail.com>
2022-06-06 15:51:23 -0400

    Update tutorial004.py
    The function get_password_hash was never called, but it seemed like it was originally supposed to be called within the authenticate_user function. Let me know if I'm mistaken or missing something. Thanks!

--- docs_src/security/tutorial004.py
+++ docs_src/security/tutorial004.py
@@ -70,7 +70,7 @@ def authenticate_user(fake_db, username: str, password: str):
     user = get_user(fake_db, username)
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, get_password_hash(user.hashed_password)):
         return False
     return user
 
